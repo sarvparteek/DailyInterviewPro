419589890201aa1726e7d8fa58a178b7d1495297
sarvparteek <sarvps@umich.edu>
2020-11-17 19:53:05 -0500

    Change ordering of functions

--- murder_witness_of_tall_people.py
+++ murder_witness_of_tall_people.py
@@ -24,17 +24,6 @@ Date: Nov 17, 2020
 Brief: Finds the number of witnesses arranged in a decreasing order of heights '''
 
 
-def update_witness_list(w_list, person):
-    # If there is no existing witness, or if the new person is shorter than the last witness in the list, then the list
-    # is up to date after the inclusion of this new person
-    if not w_list or person < w_list[-1]:
-        w_list.append(person)
-        return True
-    else:  # if the new person is taller than the last witness in the list, then remove that witness
-        del w_list[-1]
-        return False
-
-
 def witnesses(heights):
     assert len(heights) > 0, "No witness available"
     witness_list = []
@@ -46,6 +35,15 @@ def witnesses(heights):
     print("Witness list: ", witness_list)
     print("Number of witnesses: ", len(witness_list), "\n")
 
+def update_witness_list(w_list, person):
+    # If there is no existing witness, or if the new person is shorter than the last witness in the list, then the list
+    # is up to date after the inclusion of this new person
+    if not w_list or person < w_list[-1]:
+        w_list.append(person)
+        return True
+    else:  # if the new person is taller than the last witness in the list, then remove that witness
+        del w_list[-1]
+        return False
 
 witnesses([3, 6, 4, 1])  # Expected: [6, 4, 1] = 3 witnesses
 witnesses([4, 3, 5, 2, 6, 1, 4, 3, 1, 2, 1])  # Expected: [6, 4, 3, 2, 1] = 5 witnesses
